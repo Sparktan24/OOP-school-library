badabf93ce11f4bb4307da4d090af2fae6f35983
Jesus Molina <netod90@gmail.com>
2023-07-06 16:30:18 -0700

    Add list_all_books method

--- app.rb
+++ app.rb
@@ -17,19 +17,32 @@ class App
     books << book
     puts 'Book created successfully!'
   end
+
+  def list_all_books(books)
+    puts 'List of all books: '
+    books.each { |book| puts "Title: #{book.title}, Author #{book.author}" }
+  end
 end
 
 def main
   app = App.new
   books = []
-  puts '1. Create a book'
-  print 'Choose an option: '
-  option = gets.chomp.to_i
-  case option
-  when 1
-    app.create_book(books)
-  else
-    puts 'invalid option'
+
+  loop do
+    puts '1. Create a book'
+    puts '2. List all books'
+    print 'Choose an option: '
+    option = gets.chomp.to_i
+    case option
+    when 1
+      app.create_book(books)
+    when 2
+      app.list_all_books(books)
+    when 0
+      break
+    else
+      puts 'invalid option'
+    end
   end
 end
 
