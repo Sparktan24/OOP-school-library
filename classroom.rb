6090d69129d939b99e85eba4449fcf0ea3af22f0
Jesus Molina <netod90@gmail.com>
2023-07-05 17:27:28 -0700

    Add classroom and stundent relationships

--- classroom.rb
+++ classroom.rb
@@ -1,11 +1,17 @@
 class Classroom
-  attr_accessor :label
+  attr_accessor :label, :students
 
   def initialize
     @label
+    @students = []
+  end
+
+  def add_student(student)
+    students << student
+    student.classroom = self
   end
 end
 
 classroom = Classroom.new
-classroom.label = '100'
+classroom.label = 'A-10'
 puts classroom.label
