0bba6528bc607a283e5c6c5df7df45d819ca247b
Jesus Molina <netod90@gmail.com>
2023-07-10 19:35:05 -0700

    Update refactor menu

--- menu.rb
+++ menu.rb
@@ -3,34 +3,96 @@ require_relative 'app'
 class Menu
   def initialize
     @app = App.new
+    @options = menu_options
   end
 
   def menu_render
-    exit_program = false
-    options = {
-      1 => -> { @app.list_all_books },
-      2 => -> { @app.list_all_people},
-      3 => -> { @app.create_person },
-      4 => -> { @app.create_book },
-      5 => -> { @app.create_rental },
-      6 => -> { @app.list_rents_per_id },
-      0 => -> { exit_program = true },
-      default: -> { puts 'Invalid option' }
-    }
-
     loop do
-      puts '1. List all books'
-      puts '2. List all peple'
-      puts '3. Create a person'
-      puts '4. Create a book'
-      puts '5. Create a rental'
-      puts '6. list rents person(ID)'
-      puts '0. To exit'
-      print 'Choose an option: '
-      option = gets.chomp.to_i
+      display_menu
+      option = option_choice
 
-      options[option]&.call || options[:default]&.call
-      break if exit_program
+      break if option.zero?
+
+      print 'Choose an option: '
+      execute_chosen_option(option)
     end
+  end
+
+  def menu_options
+    [
+      { option: ListAllBooksOption.new(@app), description: 'List all books' },
+      { option: ListAllPeopleOption.new(@app), description: 'List all people' },
+      { option: CreatePersonOption.new(@app), description: 'Create a person' },
+      { option: CreateBookOption.new(@app), description: 'Create a book' },
+      { option: CreateRentalOption.new(@app), description: 'Create a rental' },
+      { option: ListRentsPerIdOption.new(@app), description: 'List rents per person ID' }
+    ]
+  end
+
+  def display_menu
+    puts 'Menu:'
+    @options.each_with_index { |option, index| puts "#{index + 1}. #{option[:description]}" }
+    puts '0. To exit'
+  end
+
+  def option_choice
+    print 'Choose an option: '
+    gets.chomp.to_i
+  end
+
+  def execute_chosen_option(option)
+    selected_option = @options[option - 1]
+    if selected_option.nil?
+      puts 'Not a valid option'
+    else
+      selected_option[:option].execute
+    end
+  end
+end
+
+class MenuOptions
+  def initialize(app)
+    @app = app
+    @name = self.class.name
+  end
+
+  def execute
+    raise NotImplementedError, 'Subclasses must implement the execute method'
+  end
+end
+
+class ListAllBooksOption < MenuOptions
+  def execute
+    @app.list_all_books
+  end
+end
+
+class ListAllPeopleOption < MenuOptions
+  def execute
+    @app.list_all_people
+  end
+end
+
+class CreatePersonOption < MenuOptions
+  def execute
+    @app.create_person
+  end
+end
+
+class CreateBookOption < MenuOptions
+  def execute
+    @app.create_book
+  end
+end
+
+class CreateRentalOption < MenuOptions
+  def execute
+    @app.create_rental
+  end
+end
+
+class ListRentsPerIdOption < MenuOptions
+  def execute
+    @app.list_rents_per_id
   end
 end
