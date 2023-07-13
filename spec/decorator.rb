dd67cac86b7772fa9fb7cdfcd49c0f36542d8fde
Jesus Molina <netod90@gmail.com>
2023-07-13 10:43:36 -0700

    Fix linter

--- spec/decorator.rb
+++ spec/decorator.rb
@@ -10,12 +10,3 @@ describe Decorator do
     end
   end
 end
-
-# person = Person.new(22, 'maximilianus')
-# puts person.correct_name
-# capitalized_person = CapitalizeDecorator.new(person)
-# puts capitalized_person.correct_name
-# capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-# puts capitalized_trimmed_person.correct_name
-# puts person.can_use_services?
-# puts person.correct_name
