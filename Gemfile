566b6afcc9b84eedfc68fa33065225fb196808d6
Jesus Molina <netod90@gmail.com>
2023-07-13 09:58:32 -0700

    Add teacher test

--- Gemfile
+++ Gemfile
@@ -1,1 +1,3 @@
-gem 'rubocop', '>= 1.0', '< 2.0'
+# gem 'rubocop', '>= 1.0', '< 2.0'
+gem 'rubocop', '1.54.2'
+gem 'rspec', '~> 3.0' # >= 3.0 and < 4.0s
--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -1,1 +1,11 @@
 require_relative '../teacher'
+
+describe Teacher do
+  # specialization = Classroom.new
+  describe 'can_use_services? method' do
+    it 'returns permission true' do
+      teacher = Teacher.new(39, 'Mage', 'Larry', parent_permission: true)
+      expect(teacher.can_use_services?).to eq(true)
+    end
+  end
+end
